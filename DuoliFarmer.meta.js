94c6f73e9928e2c5c9a26a7d44d0dbc765734031
kudodz <154000395+kudo1004@users.noreply.github.com>
2025-05-27 18:05:15 +0700

    meta

---
+++ DuoliFarmer.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         DuoliFarmer
+// @namespace    https://tampermonkey.net
+// @version      1.0
+// @description  DuoliFarmer giúp bạn tăng Xp Duolingo siêu nhanh!
+// @author       kudodz
+// @match        https://*.duolingo.com/*
+// @grant        none
+// @license      none
+// ==/UserScript==
